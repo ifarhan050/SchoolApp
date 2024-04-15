e8926d011bc52b75756c0fc27cabc9560b866530
DELL <farhan.irfan@braincrop.net>
2024-04-15 11:58:03 +0500

    fix: change files

--- DemoAttendenceFeature/Infrastructure/Interface/ITokenRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace DemoAttendenceFeature.Infrastructure.Interface
-{
-    public interface ITokenRepository
-    {
-        string CreateToken(IdentityUser user, List<string> roles);
-    }
-}
